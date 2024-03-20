683c9eb3dac06d901e95619fc051a3170d7c6e41
Felipe7771 <126116835+Felipe7771@users.noreply.github.com>
2024-03-20 18:14:46 -0300

    Add files via upload

--- Move_file.py
+++ Move_file.py
@@ -1,25 +1,10 @@
 import os, shutil
 
-origin = "C:/Users/User/Downloads"
-destiny = "C:/Users/User/Downloads/Arquivos_Documentos"
+from_dir = "C:/Users/User/Downloads"
+to_dir = "C:/Users/User/Downloads/P_Arquivos_Documentos"
 
-list_file = os.listdir(origin)
+list_of_files = os.listdir(from_dir)
 
-for file_name in list_file:
-    name, extation = os.path.splitext(file_name)
-
-    if extation == "":
-        continue
-    if extation in [".txt",".doc",".docx",".pdf"]:
-        path1 = origin + "/" + file_name
-        path2 = destiny
-        path3 = path2 + "/" + file_name
-
-        if os.path.exists(path2):
-            print("Movendo ",file_name,"...")
-            shutil.move(path1,path3)
-        else:
-            os.makedirs(path2)
-            print("Movendo ",file_name,"...")
-            shutil.move(path1,path3)
-
+for file in list_of_files:
+    name, extation = os.path.splitext(file)
+    print("Arquivo:",name," Extensão:",extation)
